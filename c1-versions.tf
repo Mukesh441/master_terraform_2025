fde2064732974ea0b702a1973ab80705ebcc0bb6
Mukesh441 <sethi.mukeshjain@gmail.com>
2025-08-28 11:21:39 +0530

    Terraform formated

--- c1-versions.tf
+++ c1-versions.tf
@@ -1,7 +1,7 @@
 # Terraform Settings Block
 terraform {
   required_version = ">=  1.13"
-  required_providers {  
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0" # Optional but recommended in production
@@ -12,6 +12,6 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  profile = var.profile //"default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  profile = var.profile    //"default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = var.aws_region //"ap-south-1"
 }
--- c2-generic_variables.tf
+++ c2-generic_variables.tf
@@ -13,14 +13,14 @@ variable "profile" {
 # Environment Variable
 variable "environment" {
   description = "Environment Variable used as a prefix"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 # Business Division
 variable "business_divsion" {
   description = "Business Division in the large organization this Infrastructure belongs"
-  type = string
-  default = "SAP"
+  type        = string
+  default     = "SAP"
 }
 
 /*variable "ami_id" {
